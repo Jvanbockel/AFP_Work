6b51d6c56e6f0238830a79a1871d3eed1b80dc79
18jvanbockel <18jvanbockel@mlmc.edu.au>
2017-11-21 12:37:48 +1100

    Was taught string formating

--- Pycharm_Test.py
+++ Pycharm_Test.py
@@ -1,1 +1,13 @@
-Hi
+
+name = input("You?: ")
+age = input("Age?: ")
+
+#Comma simply outputs the result
+print("Hello", name , age)
+
+#The + tries to construst a string to display
+#and it will only work with int if they are
+#converted to string first
+print("Hello"+ name + str(age))
+
+print("Hello {0} you are {1} years old".format(name,age))
