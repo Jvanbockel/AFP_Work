3bb22c38488fa74275bc85f0582aa117d43a9c67
18jvanbockel <18jvanbockel@mlmc.edu.au>
2017-11-27 14:38:49 +1100

    When the user enters the relevant data, the code figures out the man, min average and total value by using lists and the split function.

---
+++ Monthly_Rainfall.py
@@ -0,0 +1,28 @@
+rainfall = []
+
+values = input("Enter the average rainfall in your area"
+                 " for each month. Separate values with commas: ")
+
+rainFall = values.split(',')
+print(rainFall)
+
+print("Max rainfall is: ",max(rainFall))
+print("Min rainfall is: ",min(rainFall))
+
+total = 0
+for x in rainFall:
+    total += int(x)
+
+for i in range(len(rainFall)):
+    len = int(rainFall[i])
+
+sum = 0
+for x in rainFall:
+    sum += int(x)
+
+average = sum / len
+print("The average rainfall is: ",round(average,2))
+print("The total rainfall is: ",total)
+
+
+
